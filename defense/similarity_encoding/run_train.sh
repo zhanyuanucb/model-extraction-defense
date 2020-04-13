f72b72734d54409b8d9e1d8280544325cdfdb858
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2020-04-13 09:13:42 +0000

    mnist similarity encoder

--- defense/similarity_encoding/run_train.sh
+++ defense/similarity_encoding/run_train.sh
@@ -10,8 +10,7 @@
 #python train.py --ckpt_suffix=feat --load_pretrained=1 --margin=100
 #python train.py --ckpt_suffix=feat --load_pretrained=1 --margin=1000
 
-#python train.py --ckpt_suffix=feat --load_pretrained=0 --margin=100
-python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --train_epochs=5 --sim_epochs=10 --margin=1
-#python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --margin=10
-#python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --margin=100
-#python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --margin=1000
+python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --train_epochs=5 --sim_epochs=50 --margin=1
+python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --train_epochs=5 --sim_epochs=50 --margin=10
+python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --train_epochs=5 --sim_epochs=50 --margin=100
+python train.py --dataset_name=MNIST --ckpt_suffix=mnist_feat --load_pretrained=1 --train_epochs=5 --sim_epochs=50 --margin=1000
